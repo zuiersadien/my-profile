8defe7bf8e17ad5ef4218200cc4edc9748ad915a
zuiersadien <zuiersadien@gmail.com>
2022-03-23 11:10:02 -0500

    segunda 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import lottie from 'lottie-web'
-import './app.css'
+import lottie from "lottie-web";
+import "./app.css";
 
+import React, { useEffect, useRef } from "react";
 
 function App() {
-  const app=document.getElementById('lot')
-
-  lottie.loadAnimation({
-    wrapper: app,
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'https://assets7.lottiefiles.com/packages/lf20_j6nmheu0.json' // the path to the animation json
+  const container = useRef(null);
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer:"svg",
+      loop: true,
+      autoplay: true,
+      path:'https://assets2.lottiefiles.com/packages/lf20_u4yrau.json'
+    });
   });
-  
+
   return (
-    <div className="App" id="lot">
-    asdsadsaasdsdfasdf
+    <div className="App" id="lot" ref={container}>
+      asdsadsaasdsdfasdf
     </div>
   );
 }
